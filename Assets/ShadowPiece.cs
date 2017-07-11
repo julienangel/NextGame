4fa9ca5f6bfc86e191a0d862da397e0cab1fabeb
julienangel <joaoaraujo241991@gmail.com>
2017-07-11 16:44:47 +0100

    Ad Mob added, piece with shadows now

---
+++ Assets/ShadowPiece.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPiece : MonoBehaviour {
+
+    public SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.5f);
+	}
+}
